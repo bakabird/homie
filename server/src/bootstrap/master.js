1f8cb351864da150332a517b0843b2e4f18167af
bakabird <bakabird@sina.cn>
2018-07-05 21:58:53 +0800

    set the sqlite3 init

--- server/src/bootstrap/master.js
+++ server/src/bootstrap/master.js
@@ -1,1 +1,9 @@
 // invoked in master
+const fs = require('fs')
+const sqlite3 = require('sqlite3').verbose()
+const path = require('path')
+// 确认需要的数据库存在
+fs.mkdirSync(path.join(think.ROOT_PATH, 'runtime/sqlite/'))
+const db = new sqlite3.Database(path.join(think.ROOT_PATH, 'runtime/sqlite/homie'));
+// 确认需要的数据表存在
+// 房间数据库
