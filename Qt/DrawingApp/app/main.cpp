4f19811abab865f9bd7f2006621824ec1349487c
wantao <13693268858@163.com>
2018-07-08 23:06:30 +0800

    add a process

--- Qt/DrawingApp/app/main.cpp
+++ Qt/DrawingApp/app/main.cpp
@@ -3,14 +3,23 @@
 #include <exception>
 #include <QDebug>
 
+#include <QProcess>
+
+
 int main(int argc, char *argv[])
 {
     int status;
 
     try {
         QApplication a(argc, argv);
+        QProcess nodeserver;
+
+        nodeserver.start("node development.js");
+
         MainWindow w;
-        w.show();
+        w.show(" ");
+
+
         status = a.exec();
     } catch (std::exception &e) {
         qDebug() << e.what();
